7ad1ebddaad7ee193713adca265efd18a7019c21
Milos Vasic <milos85vasic@gmail.com>
2021-01-29 22:05:26 +0100

    Run script.

--- run.sh
+++ run.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
 
+config_file="configuration.txt"
+report_script="generate_report.sh"
+
+if test -e "$config_file"; then
+
+  directories=$(cat "$config_file")
+  for directory in $directories; do
+
+    if test -e "$report_script"; then
+
+      echo "Generating report for: $directory"
+      sh "$report_script" "$directory"
+    else
+
+      echo "ERROR: Could not find '$report_script'"
+      exit 1
+    fi
+  done
+else
+
+  echo "ERROR: Could not find '$config_file'"
+  exit 1
+fi
